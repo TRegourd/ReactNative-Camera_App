16d2d782e3904cb383585657c5c2555ce7e78d45
Thomas Regourd <tom.reg@hotmail.fr>
2022-04-29 16:06:28 +0200

    created camera app

--- App.js
+++ App.js
@@ -1,20 +1,161 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
+import { Camera } from "expo-camera";
+import * as FaceDetector from "expo-face-detector";
+import * as MediaLibrary from "expo-media-library";
+
+//Icons
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function App() {
+  const [hasCameraPermission, setHasPermission] = useState(null);
+  const [hasMediaPermission, setHasMediaPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [faceDetected, setFaceDetected] = useState(false);
+  const [facePosition, setFacePosition] = useState();
+  const ref = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await MediaLibrary.requestPermissionsAsync();
+      setHasMediaPermission(status === "granted");
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const handleFacesDetected = ({ faces }) => {
+    if (faces.length != 0) {
+      setFaceDetected(true);
+      setFacePosition(faces[0].bounds);
+    } else {
+      setFaceDetected(false);
+    }
+  };
+
+  const displayFaceDetected = () => {
+    if (faceDetected) {
+      return (
+        <View
+          style={{
+            flex: 1,
+            borderColor: "red",
+            borderWidth: 5,
+            position: "absolute",
+            top: facePosition.origin.y,
+            left: facePosition.origin.x,
+            height: facePosition.size.height,
+            width: facePosition.size.width,
+          }}
+        ></View>
+      );
+    } else {
+      return <View></View>;
+    }
+  };
+
+  const onPressSnapButton = async () => {
+    let options = {
+      quality: 1,
+    };
+
+    if (ref && hasMediaPermission) {
+      const PictureData = await ref.current.takePictureAsync(options);
+      MediaLibrary.saveToLibraryAsync(PictureData.uri);
+    } else {
+      console.log("pas d'accès aux medias");
+    }
+  };
+
+  if (hasCameraPermission === null) {
+    return <View />;
+  }
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Camera
+        ratio="16:9"
+        style={styles.camera}
+        type={type}
+        ref={ref}
+        onFacesDetected={handleFacesDetected}
+        faceDetectorSettings={{
+          mode: FaceDetector.FaceDetectorMode.fast,
+          detectLandmarks: FaceDetector.FaceDetectorLandmarks.none,
+          runClassifications: FaceDetector.FaceDetectorClassifications.none,
+          minDetectionInterval: 10,
+          tracking: true,
+        }}
+      >
+        <View style={styles.flipButtonContainer}>
+          <TouchableOpacity
+            style={styles.flipButton}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <MaterialIcons
+              style={styles.flipIcon}
+              name="flip-camera-ios"
+              size={50}
+              color="white"
+            />
+          </TouchableOpacity>
+        </View>
+        {displayFaceDetected()}
+        <View style={styles.snapButtonContainer}>
+          <TouchableOpacity
+            style={styles.snapButton}
+            onPress={onPressSnapButton}
+          >
+            <MaterialIcons name="camera" size={50} color="white" />
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    position: "relative",
+  },
+
+  flipButtonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    top: 20,
+    alignItems: "flex-start",
+  },
+  flipIcon: {},
+  camera: {
+    flex: 1,
+    padding: 10,
+  },
+  snapButtonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    bottom: 20,
+    alignItems: "flex-end",
   },
 });
